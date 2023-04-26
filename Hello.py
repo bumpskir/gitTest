891cc63b4937a7183e336f465cab4d14900c14a2
bumpskir <97990164+bumpskir@users.noreply.github.com>
2023-04-26 12:19:41 -0400

    Update Hello.py

--- Hello.py
+++ Hello.py
@@ -1,4 +1,5 @@
 # this is a test of creating a file in git
 name = input("What is your name? ").title().strip()
+day = input("What day is it? ")
 
-print(f"How is it going {name}")
+print(f"How is it going {name}, it is {day} today")
