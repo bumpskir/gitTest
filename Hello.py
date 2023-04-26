f6e30ee2b72934fa5ad4935e9b74de41e5547652
bumpskir <wdavern@gmail.com>
2023-04-26 12:53:43 -0400

    added more to the print statement

--- Hello.py
+++ Hello.py
@@ -2,4 +2,4 @@
 name = input("What is your name? ").title().strip()
 day = input("What day is it? ")
 
-print(f"How is it going {name}, it is {day} today")
+print(f"How is it going {name}, it is {day} today, hope you have a great day {name}")
