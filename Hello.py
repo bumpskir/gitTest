d3f8aee8c045eb6771ba6f8a34d3f7ff4929105c
bumpskir <wdavern@gmail.com>
2023-04-26 11:54:58 -0400

    I fixed the python input statement.  This is a message

--- Hello.py
+++ Hello.py
@@ -1,3 +1,4 @@
 # this is a test of creating a file in git
-name = input("What is your name? ")
-print(f"How is it going {name}").title().strip()
+name = input("What is your name? ").title().strip()
+
+print(f"How is it going {name}")
