07d0171384306543fc5ec9197cfe87e641a1bb4e
bumpskir <97990164+bumpskir@users.noreply.github.com>
2023-04-26 12:58:17 -0400

    Update Hello.py

--- Hello.py
+++ Hello.py
@@ -1,5 +1,7 @@
 # this is a test of creating a file in git
+# input to get the users name
 name = input("What is your name? ").title().strip()
+# input to get the day of the week
 day = input("What day is it? ")
 
 print(f"How is it going {name}, it is {day} today, hope you have a great day {name}")
