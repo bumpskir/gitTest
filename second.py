6092df7f350b7eade8a0122ca25deae53cfb1045
bumpskir <97990164+bumpskir@users.noreply.github.com>
2023-07-14 14:05:25 -0400

    Updated. with an age input

--- second.py
+++ second.py
@@ -1,3 +1,4 @@
 me = input("what is your name ").strip().title()
 print(f"hello {me}\n")
-
+age = input("what is your age ?").strip.title()
+print(f"Wow, you are {age} ?  you don't look a day over {int(age) - 1}")
