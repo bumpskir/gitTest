96dbdb461af2b6d25f7f3019c97ef4c458409bc1
Bill Davern <billdavern@Snows-MacBook-Pro.local>
2023-07-14 14:45:14 -0400

    looks

--- second.py
+++ second.py
@@ -1,4 +1,4 @@
 me = input("what is your name ").strip().title()
 print(f"hello {me}\n")
-age = input("what is your age ?").strip.title()
-print(f"Wow, you are {age} ?  you don't look a day over {int(age) - 1}")
+age = input("what is your age ?").title().strip()
+print(f"Wow, you are {age} ?  You don't look a day over {int(age) - 1}")
