e545cf9594e5b05265461906247da0124a7fb5a6
Bill Davern <billdavern@Snows-MacBook-Pro.local>
2023-07-14 14:08:17 -0400

    updated Hello.py

--- Hello.py
+++ Hello.py
@@ -4,6 +4,6 @@ name = input("What is your name? ").title().strip()
 # input to get the day of the week
 day = input("What day is it? ")
 # finish touch
-name2 = "HomeSlice"
+name2 = "HomieSlice"
 # This is just displaying what was input with a message
 print(f"How is it going {name}, it is {day} today, hope you have a great day {name2}!!!")
