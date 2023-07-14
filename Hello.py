97279b2962c281deda5fd8121ae897ee35204c47
Bill Davern <billdavern@Snows-MacBook-Pro.local>
2023-07-14 13:11:36 -0400

    Added HomeSlice Quote

--- Hello.py
+++ Hello.py
@@ -3,6 +3,7 @@
 name = input("What is your name? ").title().strip()
 # input to get the day of the week
 day = input("What day is it? ")
-
+# finish touch
+name2 = "HomeSlice"
 # This is just displaying what was input with a message
-print(f"How is it going {name}, it is {day} today, hope you have a great day {name}!!!")
+print(f"How is it going {name}, it is {day} today, hope you have a great day {name2}!!!")
